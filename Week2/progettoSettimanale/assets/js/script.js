06b9627f884a8e04cd09d3c55856c87bc1b13a90
“StefaniaFracasso <fracasso.stefania@gmail.com>
2023-02-10 12:02:43 +0100

    week2 progettoSettimanale

---
+++ Week2/progettoSettimanale/assets/js/script.js
@@ -0,0 +1,52 @@
+var elemento1 = 0;
+var elemento2 = 0;
+var operatore;
+
+function inputNumber (digit) {
+  document.getElementById('inputField').value += digit
+}
+
+function memorizzaOperazione(operator) {
+  elemento1 = document.getElementById('inputField').value;
+  document.getElementById('inputField').value = "";
+  operatore = operator
+}
+
+function memorizzaElemento2 () {
+  elemento2 = document.getElementById('inputField').value;
+  document.getElementById('inputField').value = "";
+  calcola ();
+}
+
+function calcola () {
+  elemento1 = Number (elemento1);
+  elemento2 = Number (elemento2);
+
+  switch (operatore) {
+		case '+':
+			risultato = elemento1 + elemento2;
+			break;
+		case '-':
+			risultato = elemento1 - elemento2;
+			break;
+		case '*':
+			risultato = elemento1 * elemento2;
+			break;
+		case '/':
+			risultato = elemento1 / elemento2;
+			break;
+		default:
+			risultato = 0;
+			break;
+	}
+
+  document.getElementById('inputField').value = risultato;
+  elemento1 = 0;
+  elemento2 = 0;
+}
+
+function cancellaField (canc) {
+    document.getElementById('inputField').value = "";
+    elemento1 = 0;
+    elemento2 = 0;
+}
