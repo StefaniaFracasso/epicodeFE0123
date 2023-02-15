53cef9d2ec128880a0b7fabd0096eaffdac09b88
“StefaniaFracasso <fracasso.stefania@gmail.com>
2023-02-15 09:27:22 +0100

    week3 G2

--- Week3/G2/assets/js/script.js
+++ Week3/G2/assets/js/script.js
@@ -2,15 +2,19 @@
 Scrivi una funzione per concatenare due stringhe ricevute come parametri, selezionando solamente i primi 2 caratteri della
 prima e gli ultimi 3 della seconda. Converti la stringa risultante in maiuscolo e mostrala con un console.log().
 */
-let str1 = 'Ciao';
-let str2 = 'mondo';
-function concatenaStringhe(str1, str2) {
-    str1 = str1.slice(0, 2);
-    str2 = str2.slice(-3);
-    strConc = str1.concat(str2);
-    return strConc.toUpperCase();
-};
-console.log(concatenaStringhe(str1, str2));
+
+var stringa1 = "ciao";
+var stringa2 = "a tutti";
+var nuovaStringa = "";
+
+function concatenaStringa (stringa1, stringa2) {
+    var parte1 = stringa1.substr(0, 2);
+    var parte2 = stringa2.substr(-3);
+    var nuovaStringa = parte1 + parte2;
+    return nuovaStringa;
+}
+console.log(concatenaStringa(stringa1, stringa2).toUpperCase());
+
 /* ESERCIZIO 2
 Scrivi una funzione per creare un array di soli valori DISPARI da 1 a 100.
 */
@@ -21,6 +25,7 @@ while (i < 100) {
     i = i + 2;
 }
 console.log(oddArray);
+
 /* ESERCIZIO 3
 Scrivi una funzione per creare un array di soli valori multipli di 5, da 1 a 100.
 */
@@ -45,6 +50,7 @@ function aggiungi10() {
     } return array10;
 }
 console.log(aggiungi10());
+
 /* ESERCIZIO 5
 Scrivi una funzione per ricavare solamente i valori PARI da un array composto da soli valori numerici.
 */
@@ -63,6 +69,24 @@ console.log(dammiPari());
 /* ESERCIZIO 6
 Scrivi una funzione per sommare a catena i numeri contenuti in un array.
 */
+
+var arr = [];
+const numeroElementi = 3;
+const max = 10;
+
+for(let i = 0; i<numeroElementi; i++){
+    arr.push(Math.floor(Math.random() *max))
+}
+console.log(arr)
+
+function sommaArray (arr) {
+    var somma = 0;
+    for (let i = 0; i < arr.length; i++) {
+        somma += arr[i];
+    }return somma;
+}
+somma = sommaArray (arr)
+console.log(somma)
 
 /* ESERCIZIO 7
 Scrivi una funzione per incrementare di 1 tutti i valori numerici in un array.
@@ -87,6 +111,26 @@ es: [1, 3, 5] ==> [5, 3, 1]
 */
 
 /* Questo array di film verrà usato negli esercizi a seguire. Non modificarlo e scorri oltre per riprendere gli esercizi :) */
+
+/* ESERCIZIO 12
+Scrivi una funzione per trovare il film più vecchio nell'array fornito.
+*/
+
+/* ESERCIZIO 13
+Scrivi una funzione per ottenere il numero di film contenuti nell'array fornito.
+*/
+
+/* ESERCIZIO 14
+Scrivi una funzione per creare un array con solamente i titoli dei film contenuti nell'array fornito.
+*/
+
+/* ESERCIZIO 15
+Scrivi una funzione per ottenere dall'array fornito solamente i film usciti nel millennio corrente.
+*/
+
+/* ESERCIZIO 16
+Scrivi una funzione per ottenere dall'array fornito uno specifico film (la funzione riceve un imdbID come parametro).
+*/
 const movies = [
     {
         Title: 'The Lord of the Rings: The Fellowship of the Ring',
@@ -202,45 +246,33 @@ const movies = [
     },
 ]
 
-/* ESERCIZIO 12
-Scrivi una funzione per trovare il film più vecchio nell'array fornito.
-*/
-
-/* ESERCIZIO 13
-Scrivi una funzione per ottenere il numero di film contenuti nell'array fornito.
-*/
-
-/* ESERCIZIO 14
-Scrivi una funzione per creare un array con solamente i titoli dei film contenuti nell'array fornito.
-*/
-
-/* ESERCIZIO 15
-Scrivi una funzione per ottenere dall'array fornito solamente i film usciti nel millennio corrente.
-*/
-
-/* ESERCIZIO 16
-Scrivi una funzione per ottenere dall'array fornito uno specifico film (la funzione riceve un imdbID come parametro).
-*/
-
-
-let bottone = document.getElementById('myBtn');
-let filmTrovato = document.getElementById('filmTrovato').innerHTML;
+var bottone = document.getElementById('myBtn');
+var risultatoFinale = document.getElementById('filmTrovato');
 
 window.addEventListener('load', function() {for (let i = 0; i < movies.length; i++) {
     document.getElementById('select').innerHTML += `<option value="${movies[i].imdbID}">${movies[i].Title}</option>`;
 }});
 
-bottone.addEventListener('click', getMovieById());
-
-function getMovieById() {
-    for (let i = 0; i < movies.length; i++) {
-        let filmScelto = document.getElementById('select').value;
-        if (filmScelto == movies[i].imdbID) {
-            filmTrovato = movies[i];
-        } else {
-            filmTrovato = 'Seleziona un valore';
+function trovaFilm () {
+    var idSelezionato = document.getElementById('select').value;
+    for (i=0; i< movies.length; i++) {
+        if (idSelezionato == movies[i].imdbID) {
+            return movies[i]
         }
-  }};
+    }
+} 
+
+bottone.addEventListener ('click', function (){
+    var filmTrovato = trovaFilm ()
+    if (filmTrovato != undefined){
+     risultatoFinale.innerHTML = `<h2>Il film scelto è </h2><h3>${filmTrovato.Title}</h3><h3>L'anno di uscita è il ${filmTrovato.Year}</h3><img src="${filmTrovato.Poster}">`;
+    }
+    else {
+     risultatoFinale.innerHTML = "Il film selezionato non esiste"
+    }
+
+})
+
 /* ESERCIZIO 17
 Scrivi una funzione per calcolare la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array fornito.
 */
@@ -248,3 +280,28 @@ Scrivi una funzione per calcolare la somma di tutti gli anni in cui sono stati p
 /* ESERCIZIO 18 (EXTRA)
 Scrivi una funzione per recuperare tutti i film dall'array fornito che contengono una parola fornita.
 */
+
+var searchBar = document.getElementById('text')
+var searchButton = document.getElementById('cerca')
+
+searchButton.addEventListener ('click' , function (){
+    inputMovie ();
+})
+
+function inputMovie () {
+    var userInput = searchBar.value.toLowerCase();
+    var risultatiRicerca = document.getElementById('risultatiRicerca')
+    var found = false; //prima di iniziare a controllare i film nel ciclo for, imposto una variabile su false perchè non ho ancora trovato nulla. la variabile resta su false finchè non trovo una corrispondente che la faccia diventare vera
+    risultatiRicerca.innerHTML = '';
+
+    for (i = 0; i < movies.length; i++) {
+        var movieTitle = movies[i].Title.toLowerCase();
+        if (movieTitle.indexOf(userInput) >=0 ) {
+            risultatiRicerca.innerHTML += `<h2>I film disponibili sono:</h2><h3>${movies[i].Title}</h3><h3>L'anno di uscita è il ${movies[i].Year}</h3><img src="${movies[i].Poster}">`;
+            found = true // dal momento che il ciclo ha trovato almeno una corrispondenza, posso dichiarare che la variabile è vera
+        }
+    }
+    if (found === false) { // dopo aver cercato in tutti gli oggetti degli array e verificato che found sia stato sempre falso e che quindi non abbia mai trovato nulla, scriviamo che il film ricercato non è disponibile
+        risultatiRicerca.innerHTML = "Il film ricercato non è disponibile"
+    }
+}
